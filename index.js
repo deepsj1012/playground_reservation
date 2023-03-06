48b04b0d8844733796cdc43da8c1923413b601a3
deepsj1012 <deepsj1012@naver.com>
2023-03-06 14:59:53 +0900

    project init & build

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+const app = express()
+
+
+
+
+
+const port = 9999
+app.listen(port, console.log("server started"))
+
+
+
+//코드 빌드 순서
+//1. 위에서 아래 순서로 빌드를 한다.
+//2. = 를 기준으로 오른쪽에서 왼쪽으로 치환한다.
+//3. .은 하위 메소드를 호출하는 방법.
+//4. ()는 함수들의 모음을 의미한다.
+//5. ,는 그리고로 해석한다.
